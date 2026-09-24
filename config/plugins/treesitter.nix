017acfeff0f5493ace3009a9fd2f770007d23cf5
Sly-Harvey <86175780+Sly-Harvey@users.noreply.github.com>
2026-09-24 16:39:53 +0100

    Added treesitter-textobjects

--- config/plugins/treesitter.nix
+++ config/plugins/treesitter.nix
@@ -1,7 +1,114 @@
 { ... }:
 {
+  keymaps = [
+    {
+      mode = [
+        "x"
+        "o"
+      ];
+      key = "af";
+      action = ''<cmd>lua require "nvim-treesitter-textobjects.select".select_textobject("@function.outer", "textobjects")<cr>'';
+    }
+    {
+      mode = [
+        "x"
+        "o"
+      ];
+      key = "if";
+      action = ''<cmd>lua require "nvim-treesitter-textobjects.select".select_textobject("@function.inner", "textobjects")<cr>'';
+    }
+    {
+      mode = [
+        "x"
+        "o"
+      ];
+      key = "ac";
+      action = ''<cmd>lua require "nvim-treesitter-textobjects.select".select_textobject("@class.outer", "textobjects")<cr>'';
+    }
+    {
+      mode = [
+        "x"
+        "o"
+      ];
+      key = "ic";
+      action = ''<cmd>lua require "nvim-treesitter-textobjects.select".select_textobject("@class.inner", "textobjects")<cr>'';
+    }
+    {
+      mode = [
+        "x"
+        "o"
+      ];
+      key = "as";
+      action = ''<cmd>lua require "nvim-treesitter-textobjects.select".select_textobject("@local.scope", "locals")<cr>'';
+    }
+    {
+      mode = [
+        "x"
+        "o"
+      ];
+      key = "aa";
+      action = ''<cmd>lua require("nvim-treesitter-textobjects.select").select_textobject("@parameter.outer", "textobjects")<cr>'';
+    }
+    {
+      mode = [
+        "x"
+        "o"
+      ];
+      key = "ia";
+      action = ''<cmd>lua require("nvim-treesitter-textobjects.select").select_textobject("@parameter.inner", "textobjects")<cr>'';
+    }
+    {
+      mode = [
+        "x"
+        "o"
+      ];
+      key = "ai";
+      action = ''<cmd>lua require("nvim-treesitter-textobjects.select").select_textobject("@conditional.outer", "textobjects")<cr>'';
+    }
+    {
+      mode = [
+        "x"
+        "o"
+      ];
+      key = "ii";
+      action = ''<cmd>lua require("nvim-treesitter-textobjects.select").select_textobject("@conditional.inner", "textobjects")<cr>'';
+    }
+    {
+      mode = [
+        "x"
+        "o"
+      ];
+      key = "at";
+      action = ''<cmd>lua require("nvim-treesitter-textobjects.select").select_textobject("@comment.outer", "textobjects")<cr>'';
+    }
+  ];
+  extraConfigLua = ''
+    require("nvim-treesitter-textobjects").setup {
+      select = {
+        lookahead = true,
+        selection_modes = {
+          -- ['@parameter.outer'] = 'v', -- charwise
+          -- ['@function.outer'] = 'V', -- linewise
+          -- ['@class.outer'] = '<c-v>', -- blockwise
+        },
+        include_surrounding_whitespace = false,
+      },
+    }
+  '';
   plugins = {
-    treesitter-context.enable = false;
+    treesitter-textobjects = {
+      enable = true;
+      # settings = {
+      #   select = {
+      #     enable = true;
+      #     lookahead = true;
+      #     include_surrounding_whitespace = true;
+      #     selection_modes = {
+      #       "@comment.outer" = "V";
+      #     };
+      #   };
+      # };
+    };
     treesitter = {
       enable = true;
       nixvimInjections = true;
@@ -9,7 +116,6 @@
       folding.enable = false;
       settings = {
         indent.enable = true;
-        # incremental_selection.enable = true;
         incremental_selection = {
           enable = true;
           keymaps = {
@@ -20,8 +126,9 @@
           };
         };
       };
+
       /*
-        grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
+        grammarPackages = with config.plugins.treesitter.package.builtGrammars; [
           nix
           bash
           cmake
